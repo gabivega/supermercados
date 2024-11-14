626d02c21d00340f66e0b8784dfb6c8012e48a55
Gabi <gabi.vega1988@gmail.com>
2024-11-13 22:51:39 -0300

    puppeteer update

--- server/index.js
+++ server/index.js
@@ -37,11 +37,11 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-// cron.schedule("40 01 * * * ", async () => {
-//     await scrapping();
-//     await updateVariations();
-//     await calcularEstadisticas()
-// });
-await scrapping();
-await updateVariations();
-await calcularEstadisticas()
+cron.schedule("30 01 * * * ", async () => {
+    await scrapping();
+    await updateVariations();
+    await calcularEstadisticas()
+});
+//await scrapping();
+//await updateVariations();
+//await calcularEstadisticas()
