5b557a1baa2f8380f9fd0bce94a3663c1401aeab
Gabi <gabi.vega1988@gmail.com>
2024-12-20 10:15:29 -0300

    cron date update

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-cron.schedule("59 12 * * * ", async () => {
+cron.schedule("20 13 * * * ", async () => {
     await scrapping();
     await updateVariations();
     await calcularEstadisticas()
